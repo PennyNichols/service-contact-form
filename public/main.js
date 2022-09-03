d738c85d40bbcd024ea8e44b606f14b1ea129487
pnichols789 <pnichols789@gmail.com>
2022-09-03 07:19:16 -0500

    trying to make form send to email and database

--- public/main.js
+++ public/main.js
@@ -41,5 +41,48 @@ var gallerySlider = tns({
     }
 });
 
+form.addEventListener('submit', sendEmail);
 
+function sendEmail() {
+  const myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+
+  const raw = JSON.stringify({
+    email: 'pnichols789@gmail.com',
+    html:
+      'First Name: ' +
+      document.getElementById('first-name').value +
+      '<br> Last Name: ' +
+      document.getElementById('last-name').value +
+      '<br> Phone Number: ' +
+      document.getElementById('phone').value +
+      '<br> Email Address: ' +
+      document.getElementById('email').value +
+      '<br> Property Address: ' +
+      document.getElementById('address').value +
+      '<br> Date: ' +
+      document.getElementById('date').value +
+      '<br> Time: ' +
+      document.getElementById('time').value +
+      '<br> Service: ' +
+      document.getElementById('service').value +
+      '<br> Message: ' +
+      document.getElementById('message').value,
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow',
+  };
+
+  fetch('https://barry-dev-mail-app.herokuapp.com/mail', requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      console.log(result);
+      alert('Appointment Request Successful');
+    })
+    .catch((error) => console.log('error', error));
+};
   
